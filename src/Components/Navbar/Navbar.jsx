ede8b9adfab08917f60f5dc8adabfcf517a91432
Major0dg <saviour4anetie@gmail.com>
2024-02-06 13:53:58 +0100

    navbar update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,24 +5,28 @@ import { profile } from "../Assets";
 const Navbar = () => {
   return (
     <nav className="text-red-600 w-full h-22 bg-white border-b-[1px] border-b-gray-800 font-titleFont">
-      <div className="max-w-screen-xl h-full mx-auto flex items-center justify-between">
+      <div className="max-w-screen-xl h-full mx-auto flex flex-row items-center justify-between">
         <div>
           <img src={profile} alt="logo" className="h-18 w-24" />
         </div>
-        <div>
+        <div style={{ display: "flex", flexDirection: "row" }}>
           <ul className="flex items-center gap-8">
             <li className="nav-li">Home</li>
             <li className="nav-li">Pages</li>
             <li className="nav-li">Shop</li>
             <li className="nav-li">Element</li>
             <li className="nav-li">Blog</li>
-            <li className="flex-row items-center cursor-pointer">
-              <BsCart />
-            </li>
-            <div>
-              <img src={profile} alt="profile-img" className="h-7 w-8" />
-            </div>
           </ul>
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <div className="flex flex-row items-center px-8 cursor-pointer">
+              <BsCart size={22} color="pink" />
+            </div>
+            <img
+              src={profile}
+              alt="profile-img"
+              className="h-7 w-8 cursor-pointer"
+            />
+          </div>
         </div>
       </div>
     </nav>
